8ecdc1bad0ca78fed2edd5e1dfcb6c2249a16d9b
Ben McLellan <s2748011@ed.ac.uk>
2025-03-02 16:27:03 +0000

    LAStools: raw to normalised las

--- lastools.ps1
+++ lastools.ps1
@@ -6,5 +6,8 @@ C:\LAStools\bin\lastile64.exe -i .\raw\*.laz -buffer 30 -odir .\tiled_w_buffer -
 # Removing noise. 2mx2m windows removing points with less than 3 neighbours. 
 C:\LAStools\bin\lasnoise64.exe -i .\tiled_w_buffer\*.las -odir .\cleaned -step 2 -isolated 3 -remove_noise -olas
 
-# Ground classification. 10m step, 10cm spike, 50cm offset. 
+# Ground classification. 10m step, 20cm spike, 40cm offset. 
 C:\LAStools\bin\lasground_new64.exe -i .\cleaned\*.las -odir .\ground -step 10 -spike 0.2 -offset 0.4 -olas
+
+# LAS normalisation, replacing Z. 
+C:\LAStools\bin\lasheight64.exe -i .\ground\*.las -odir .\normalised -olas -replace_z 
