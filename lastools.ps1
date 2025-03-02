a258d82cfd093ab402124eea21ee18ff1377f6a7
mclellanben <mclellanben4@gmail.com>
2025-03-02 17:44:28 +0000

    Update lastools.ps1

--- lastools.ps1
+++ lastools.ps1
@@ -1,13 +1,13 @@
 # Change into working directory
 
-# Merging the las files into one. 500m tiles and 30m buffer. 
+# Tiling las files. 500m tiles and 30m buffer. 
 C:\LAStools\bin\lastile64.exe -i .\raw\*.laz -buffer 30 -odir .\tiled_w_buffer -tile_size 500 -olas -flag_as_withheld
 
-# Removing noise. 2mx2m windows removing points with less than 3 neighbours. 
+# Removing noise. 2mx2m windows, removing points with less than 3 neighbours. 
 C:\LAStools\bin\lasnoise64.exe -i .\tiled_w_buffer\*.las -odir .\cleaned -step 2 -isolated 3 -remove_noise -olas
 
 # Ground classification. 10m step, 20cm spike, 40cm offset. 
 C:\LAStools\bin\lasground_new64.exe -i .\cleaned\*.las -odir .\ground -step 10 -spike 0.2 -offset 0.4 -olas
 
-# LAS normalisation, replacing Z. 
+# las normalisation, replacing Z. 
 C:\LAStools\bin\lasheight64.exe -i .\ground\*.las -odir .\normalised -olas -replace_z 
