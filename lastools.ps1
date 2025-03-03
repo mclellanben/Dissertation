45f0156dd852d39a1e56115c0ad2995c9f1d4fbf
Ben McLellan <s2748011@ed.ac.uk>
2025-03-03 14:05:59 +0000

    edited comments

--- lastools.ps1
+++ lastools.ps1
@@ -1,12 +1,12 @@
 # Change into working directory
 
-# Tiling las files. 500m tiles and 30m buffer. 
+# Tiling las files 
 C:\LAStools\bin\lastile64.exe -i .\raw\*.laz -buffer 30 -odir .\tiled_w_buffer -tile_size 500 -olas -flag_as_withheld
 
 # Removing noise. 2mx2m windows, removing points with less than 3 neighbours. 
 C:\LAStools\bin\lasnoise64.exe -i .\tiled_w_buffer\*.las -odir .\cleaned -step 2 -isolated 3 -remove_noise -olas
 
-# Ground classification. 10m step, 20cm spike, 40cm offset. 
+# Ground classification 
 C:\LAStools\bin\lasground_new64.exe -i .\cleaned\*.las -odir .\ground -step 10 -spike 0.2 -offset 0.4 -olas
 
 # Merge classified
@@ -15,7 +15,7 @@ C:\LAStools\bin\lasmerge64.exe -i .\ground\*.las -o merged.las -drop_withheld
 # DTM creation
 C:\LAStools\bin\las2dem64.exe -i merged.las -o .\ground\dtm.tif -step 0.1 -keep_class 2
 
-# LAS normalisation, replacing Z. 
+# LAS normalisation, replacing Z
 C:\LAStools\bin\lasheight64.exe -i .\ground\*.las -odir .\normalised -olas -replace_z 
 
 # Merge normalised las
