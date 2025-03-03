26a6207137008f3e40119d8ad2ff40fabc463c7e
Ben McLellan <s2748011@ed.ac.uk>
2025-03-03 13:54:35 +0000

    DTM added

--- lastools.ps1
+++ lastools.ps1
@@ -9,11 +9,17 @@ C:\LAStools\bin\lasnoise64.exe -i .\tiled_w_buffer\*.las -odir .\cleaned -step 2
 # Ground classification. 10m step, 20cm spike, 40cm offset. 
 C:\LAStools\bin\lasground_new64.exe -i .\cleaned\*.las -odir .\ground -step 10 -spike 0.2 -offset 0.4 -olas
 
+# Merge classified
+C:\LAStools\bin\lasmerge64.exe -i .\ground\*.las -o merged.las -drop_withheld
+
+# DTM creation
+C:\LAStools\bin\las2dem64.exe -i merged.las -o .\ground\dtm.tif -step 0.1 -keep_class 2
+
 # LAS normalisation, replacing Z. 
 C:\LAStools\bin\lasheight64.exe -i .\ground\*.las -odir .\normalised -olas -replace_z 
 
 # Merge normalised las
-C:\LAStools\bin\lasmerge64.exe -i .\normalised\*.las -o merged_norm.las -drop_withheld
+C:\LAStools\bin\lasmerge64.exe -i .\normalised\*.las -o .\normalised\merged_norm.las -drop_withheld
 
 # CHM creation (10cm resolution)
 C:\LAStools\bin\las2dem64.exe -i .\normalised\merged_norm.las -odir .\chm -otif -step 0.1 
